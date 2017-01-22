1b0693b15b613eb700f3a3292db828d6fe586d49
Ben Orchard <thefirstmuffinman@gmail.com>
2017-01-22 14:49:47 +0000

    other/get-duration: cleanup (now useful!)

--- other/fun/get-duration-of-music-dir.sh
+++ other/fun/get-duration-of-music-dir.sh
@@ -1,1 +1,16 @@
-ts=0 ; for f in *.ogg(N) *.mp3(N) ; do ; ts=$((ts + $(soxi -D $f))) ; soxi -d $f ; done ; echo ---$n${ts}s ; printf %02dh:%02dm:%02dsn $(($ts/3600)) $(($ts%3600/60)) $(($ts%60))
+#!/usr/bin/env bash
+#
+# Add up duration of OGGs and MP3s in a directory (easily expandable to other
+# music files).
+#
+# Requires sox, probably GNU printf.
+#
+
+for f in *.ogg *.mp3 ; do
+    ts=$((ts + $(printf '%.0f' $(soxi -D "$f"))))
+    soxi -d "$f"
+done
+
+echo "---"
+echo "${ts}s"
+printf '%02dh:%02dm:%02ds\n' $(($ts/3600)) $(($ts%3600/60)) $(($ts%60))
